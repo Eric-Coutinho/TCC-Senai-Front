f6cfbd600d61691c33c1fbeeec4dee16011ed1b2
eric-coutinho <eric.coutinhop@hotmail.com>
2024-06-25 09:05:55 -0300

    Updating navbar with BPS Cross logo

--- Front/src/components/Navbar/navbar.jsx
+++ Front/src/components/Navbar/navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import navcolors from "/nav-colors.png";
 import logobosch from "/logo-bosch.png";
+import bpscross from "/bpscross.png";
 
 import "./navbar.css";
 
@@ -11,6 +12,7 @@ function NavbarComponent() {
     <header>
       <img src={navcolors} className="nav-colors" alt="Nav Colors" />
       <nav>
+        <img src={bpscross} className="bps" alt="Bosch Logo" />
         <img src={logobosch} className="logo" alt="Bosch Logo" />
       </nav>
     </header>
