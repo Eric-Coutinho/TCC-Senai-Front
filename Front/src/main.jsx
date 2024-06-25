30af8e0f43011512fa734825f1defa3c5435ba67
eric-coutinho <eric.coutinhop@hotmail.com>
2024-06-25 10:36:55 -0300

    REALLY fixing it this time

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -6,15 +6,15 @@ import { BrowserRouter } from "react-router-dom";
 import NavbarComponent from './components/Navbar/navbar.jsx';
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <SpeedInsights />
     <Analytics />
     <BrowserRouter>
       <NavbarComponent />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </>,
 )
