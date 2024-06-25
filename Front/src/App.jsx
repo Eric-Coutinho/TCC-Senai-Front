488bc6ef20d4dc81372269bd2231c11a78f49549
eric-coutinho <eric.coutinhop@hotmail.com>
2024-06-25 10:17:23 -0300

    Trying to fix again

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -6,19 +6,12 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages'
 
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 function App() {
 
   return (
-    <>
-      <SpeedInsights />
-      <Analytics /> 
-      <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+    </Routes>
   )
 }
 
